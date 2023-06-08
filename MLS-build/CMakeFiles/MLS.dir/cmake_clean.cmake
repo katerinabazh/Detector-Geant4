02ffe6272341ba43b2822610a891e2e85628e907
katerinabazh <135614951+katerinabazh@users.noreply.github.com>
2023-06-08 15:41:33 +0300

    Add files via upload

---
+++ MLS-build/CMakeFiles/MLS.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MLS.dir/MLS.cc.o"
+  "CMakeFiles/MLS.dir/src/ActionInitialization.cc.o"
+  "CMakeFiles/MLS.dir/src/DetectorConstruction.cc.o"
+  "CMakeFiles/MLS.dir/src/EventAction.cc.o"
+  "CMakeFiles/MLS.dir/src/PhysicsList.cc.o"
+  "CMakeFiles/MLS.dir/src/PrimaryGeneratorAction.cc.o"
+  "CMakeFiles/MLS.dir/src/RunAction.cc.o"
+  "CMakeFiles/MLS.dir/src/SensitiveDetector.cc.o"
+  "CMakeFiles/MLS.dir/src/SteppingAction.cc.o"
+  "MLS"
+  "MLS.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MLS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
